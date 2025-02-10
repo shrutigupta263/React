1a46ed65ead863dec05dedf110cbf7f767194344
shruti gupta <shrutiguptaab263@gmail.com>
2025-02-10 23:59:04 +0530

    same but in proper way

--- customreact/customr.js
+++ customreact/customr.js
@@ -1,11 +1,18 @@
  function customRender( reactElement, conatiner){
-    const domElement = document.createElement
+    /*const domElement = document.createElement
     (reactElement.type)
     domElement.innerHTML = reactElement.childern
     domElement.setAttribute('href', reactElement.props.href)
     domElement.setAttribute('href', reactElement.props.target)
     conatiner.appendChild(domElement)
-
+    */
+   const domElement = document.createElement(reactElement.type)
+   domElement.innerHTML = reactElement.childern
+   for (const prop in reactElement.props){
+    if(prop === 'children') continue;
+    domElement.setAttribute(prop, reactElement.props[prop])
+   }
+   conatiner.appendChild(domElement)
  }
  const reactElement={
     type:'a',
